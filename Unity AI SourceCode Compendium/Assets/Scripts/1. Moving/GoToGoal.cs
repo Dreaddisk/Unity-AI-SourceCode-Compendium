651f7303ba3a7ca7ad0e33e059e11844231be15d
Jose M. Puebla <joey_manuel@outlook.com>
2020-02-11 18:06:41 -0500

    2.Created GoToGoal
    Created the script called GoatToGoal

---
+++ Unity AI SourceCode Compendium/Assets/Scripts/1. Moving/GoToGoal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToGoal : MonoBehaviour
+{
+    public float speed = 2.0f;  // Speed of the Character
+    public float accuracy = 0.01f;  // 
+    public Transform goal;  // Game Object that the character will pursue.
+
+    void LateUpdate()
+    {
+        this.transform.LookAt(goal.position);
+        Vector3 direction = goal.position - this.transform.position; // Substract the distance of the character 
+                                                                     //with the distance of the goal Game Object
+        
+        // Draws a Ray that makes the programmer see the direction traveled.
+        Debug.DrawRay(this.transform.position, direction, Color.red);
+        
+
+        if(direction.magnitude > accuracy)
+        {
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
