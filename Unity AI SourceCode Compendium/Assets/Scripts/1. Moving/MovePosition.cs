bb6b9469d9d27301b81f5484ffd4c86b7d8cba90
Jose M. Puebla <joey_manuel@outlook.com>
2020-02-11 18:37:55 -0500

    3. Created MovePosition
    On this commit:
    - we created the C# file MovePosition.

---
+++ Unity AI SourceCode Compendium/Assets/Scripts/1. Moving/MovePosition.cs
@@ -0,0 +1,25 @@
+﻿// In this script, we are going to create a file that makes the player 
+// move towards a respective object, and if the object changes its position
+// the player will update the trajecotry to the new object position.
+
+using UnityEngine;
+
+public class MovePosition : MonoBehaviour
+{
+    #region Variables
+    public Transform goal;  // Goal Object
+    float speed = 0.5f;     // Speed of player movement.
+    float accuracy = 1.0f;  // 
+    #endregion
+
+    void LateUpdate()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x,   // Updates the x coordiante position.
+                                        this.transform.position.y,  // We are not going to update the y position.
+                                        goal.position.z);   // Updates the z coordiante position.
+        this.transform.LookAt(lookAtGoal);
+
+        if (Vector3.Distance(transform.position, lookAtGoal) > accuracy)
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
