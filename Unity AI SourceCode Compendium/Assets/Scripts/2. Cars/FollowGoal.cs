9c105cc3bcee39a14f85226d1461c77494c8dcf7
Jose <joey_manuel@outlook.com>
2020-12-01 21:51:14 -0500

    6. Undertanding the Unity Waypoints System
    Created:
    - WaypointCircuit  script file
    - WaypointFollow script file
    - WaypoiontProgressTrackker script file
    - FollowGoal script file.

---
+++ Unity AI SourceCode Compendium/Assets/Scripts/2. Cars/FollowGoal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGoal : MonoBehaviour
+{
+    #region variables
+    // object that is the goal of the player to follow.
+    public Transform goal;
+
+    /// <summary>
+    /// speed, accuracy and rotation speed for the
+    /// player to.
+    /// </summary>
+    private float speed = 2.0f;
+    private float accuracy = 1.0f;
+    private float rotSpeed = 1.0f;
+    #endregion
+
+
+    private void LateUpdate()
+    {
+
+        Vector3 lookAtGoal = new Vector3(goal.position.x,
+            this.transform.position.y,
+            goal.position.z);
+
+        Vector3 direction = lookAtGoal - this.transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+            Quaternion.LookRotation(direction),
+            Time.deltaTime * rotSpeed);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+} // FollowGoal class
---
+++ Unity AI SourceCode Compendium/Assets/Scripts/2. Cars/WaypointFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollow : MonoBehaviour
+{
+    #region Variables
+    // WaypointCircuit class powered by the Utility class of the Unity Standard Assets.
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
+
+    private int currentWP = 0;
+
+    private float speed = 3.00f;
+    private float accuracy = 1.0f;
+    private float rotSpeed = 1.0f;
+
+    #endregion
+
+    private void LateUpdate()
+    {
+        if (circuit.Waypoints.Length == 0) return;
+
+        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].position.x,
+            this.transform.position.y,
+            circuit.Waypoints[currentWP].position.z);
+
+        Vector3 direction = lookAtGoal - this.transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+            Quaternion.LookRotation(direction),
+            Time.deltaTime * rotSpeed);
+
+        if(direction.magnitude < accuracy)
+        {
+            currentWP++;
+            if(currentWP >= circuit.Waypoints.Length)
+            {
+                currentWP = 0;
+            }
+        }
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
